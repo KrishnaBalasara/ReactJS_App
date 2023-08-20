4b876ab7eac9b4f08590971d2f042ceefbdf5883
Krishna <119056745+KrishnaBalasara@users.noreply.github.com>
2023-08-20 15:21:45 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is a Task Manager App using ReactJs. 
+            <p>You can add your task name and discription in this app and also perform delete operation.</p>
+        </div>
+    )
+}
